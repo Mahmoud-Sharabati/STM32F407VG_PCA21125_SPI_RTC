d82bbd796e6a227151226dcef2278b7d07dc6d2e
Mahmoud-Sharabati <mahmoud.a.sharabati@gmail.com>
2023-02-07 17:52:25 +0200

    Added the project files

---
+++ Core/Inc/PCA21125.h
@@ -0,0 +1,81 @@
+/*
+ * PCA21125.h
+ *
+ *  Created on: Aug 31, 2022
+ *      Author: Mahmoud
+ */
+
+#ifndef PCA21125_H_
+#define PCA21125_H_
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private macro -------------------------------------------------------------*/
+/*************************_Control Register IDs_ *************************/
+#define WRITE_CONTROL_1_REGESTER_ID		(0b00010000)
+#define READ_CONTROL_1_REGESTER_ID		(0b10010000)
+#define WRITE_CONTROL_2_REGESTER_ID		(0b00010001)
+#define READ_CONTROL_2_REGESTER_ID		(0b10010001)
+
+/********************_Control Register Values_ *******************/
+#define CONTROL_1_REGISTER_VALUE		(0b00001000)
+#define CONTROL_2_REGISTER_VALUE		(0b01010000)	// This value enable the INT pin on each second
+
+/**********************_Time and Date Registers_ **********************/
+#define READ_SECONDS_REGESTER_ID		(0b10010010)
+#define WRITE_SECONDS_REGESTER_ID		(0b00010010)
+#define MINUTES_REGESTER_ID				(0x03)
+#define HOURS_REGESTER_ID				(0x04)
+#define DAYS_REGESTER_ID				(0x05)
+#define WEEKDAY_REGESTER_ID				(0x06)
+#define MONTHS_REGESTER_ID				(0x07)
+#define YEARS_REGESTER_ID				(0x08)
+
+/*************************_Alarm Registers_ **************************/
+#define MINUTES_ALARM_REGESTER_ID		(0x09)
+#define HOUR_ALARM_REGESTER_ID			(0x0A)
+#define DAY_ALARM_REGESTER_ID			(0x0B)
+#define WEEKDAY_ALARM_REGESTER_ID		(0x0C)
+
+/*********************_CLKOUT Control Registers_ *********************/
+#define CLKOUT_CONTROL_REGESTER_ID		(0x0D)
+
+/*************************_Timer Registers_ *************************/
+#define TIMER_CONTROL_REGESTER_ID		(0x0E)
+#define COUNTDOWN_DOWN_REGESTER_ID		(0x0F)
+
+/********************_RTC Initial/Testing Current Values_ *******************/
+#define CURRENT_BCD_SECONDS				(0b10010000)			//bit 7 Clock Integrity "1 default", bits 6:4 Tens"0:5", bits 3:0 Unit"0:9"
+#define CURRENT_BCD_MINUTES				(0b01010000)			//bit 7 Unused "0", bits 6:4 Tens"0:5", bits 3:0 Unit"0:9"
+#define CURRENT_BCD_HOURS				(0b00010111)			//bits 7:6 Unused "00", bits 5:4 Tens"0:2", bits 3:0 Unit"0:9" (Note: 24 Mode is considered)
+#define CURRENT_BCD_DAYS				(0b00000001)			//bits 7:6 Unused "00", bits 5:4 Tens"0:3", bits 3:0 Unit"0:9"
+#define CURRENT_BITS_WEEKDAY			(0b00000000)			//bits 7:3 Unused "00000", bits 2:0 Weekday"0:6" start by Sunday(0) and end by Saturday(6)
+#define CURRENT_BCD_MONTHS				(0b00001000)			//bits 7:5 Unused "000", bit 4 Tens"0:1", bits 3:0 Unit"0:9"
+#define CURRENT_BCD_YEARS				(0b00100010)			//bits 7:4 Tens"0:9", bits 3:0 Unit"0:9"
+
+/* Private defines -----------------------------------------------------------*/
+
+/* Private variables ---------------------------------------------------------*/
+
+/* Private TypeDef -----------------------------------------------------------*/
+typedef struct {
+		uint8_t Seconds;
+		uint8_t Minutes;
+		uint8_t Hours;
+		uint8_t Days;
+		uint8_t Weekday;
+		uint8_t Month;
+		uint8_t Year;
+} RTC_Data;
+
+/* Private function prototypes -----------------------------------------------*/
+void PCA21125RTC_Init(SPI_HandleTypeDef *hspi, ITStatus INT_Status);
+void PCA21125RTC_WriteValue(RTC_Data* _rtc_data);
+void PCA21125RTC_SetTime();
+void PCA21125RTC_GetTime(RTC_Data* _rtc_data);
+uint8_t bcd2bin(uint8_t a);
+void RTC2ValidTime(RTC_Data*);
+void RTC_SetTime(RTC_Data* _rtc_data);
+
+#endif /* PCA21125_H_ */
---
+++ Core/Src/PCA21125.c
@@ -0,0 +1,136 @@
+/*
+ * PCA21125.c
+ *
+ *  Created on: Aug 31, 2022
+ *      Author: Mahmoud
+ */
+
+#include "PCA21125.h"
+
+#define CHECK_SEC_MIN_RANGE(X) 	(X > 59) ? X = 59 : ((X < 0)? X = 0 : X)
+#define CHECK_HOURS_RANGE(X) 	(X > 23) ? X = 23 : ((X < 0)? X = 0 : X)
+#define CHECK_DAYS_RANGE(X) 	(X > 31) ? X = 31 : ((X < 1)? X = 1 : X)
+#define CHECK_MONTHS_RANGE(X) 	(X > 12) ? X = 12 : ((X < 1)? X = 1 : X)
+#define CHECK_YEARS_RANGE(X) 	(X > 30) ? X = 30 : ((X < 23)? X = 23 : X)
+
+SPI_HandleTypeDef *PCA21125_SPI;
+
+void PCA21125RTC_Init(SPI_HandleTypeDef *hspi, ITStatus INT_Status) {
+
+	PCA21125_SPI = hspi;
+
+	/* Write control registers */
+	uint8_t TxADCBuff[3] = {0};
+	if (INT_Status) {
+		/* Enable the INT pin on each second */
+		TxADCBuff[2] = (uint8_t) (CONTROL_2_REGISTER_VALUE);	// INT on each second (ref. Table 7 Page 7)
+	}
+	else {
+		/* Disable the INT pin */
+		TxADCBuff[2] = (uint8_t) (0x00);
+	}
+	/* Fill the SPI data values */
+	TxADCBuff[0] = (uint8_t) (WRITE_CONTROL_1_REGESTER_ID);
+	TxADCBuff[1] = (uint8_t) (CONTROL_1_REGISTER_VALUE);
+
+	/* Pull the CS pin to low */
+	HAL_GPIO_WritePin(PCA21125_SPI_CS_GPIO_Port, PCA21125_SPI_CS_Pin, GPIO_PIN_RESET);
+	HAL_Delay(10);
+
+	/* Start Transmission process */
+	HAL_GPIO_WritePin(PCA21125_SPI_CS_GPIO_Port, PCA21125_SPI_CS_Pin, GPIO_PIN_SET);
+	if (HAL_SPI_Transmit(PCA21125_SPI,TxADCBuff, 3, 100) != HAL_OK) {
+		Error_Handler();
+	}
+	HAL_GPIO_WritePin(PCA21125_SPI_CS_GPIO_Port, PCA21125_SPI_CS_Pin, GPIO_PIN_RESET);
+	HAL_Delay(100);
+}
+
+void PCA21125RTC_WriteValue(RTC_Data* _rtc_data) {
+	/* Write Time and Date registers */
+	uint8_t TxADCBuff[8] = {0};
+
+	/* Fill the SPI data values */
+	/* Start with the seconds register */
+	TxADCBuff[0] = (uint8_t) (WRITE_SECONDS_REGESTER_ID);
+	TxADCBuff[1] = (uint8_t) (_rtc_data->Seconds);
+	TxADCBuff[2] = (uint8_t) (_rtc_data->Minutes);
+	TxADCBuff[3] = (uint8_t) (_rtc_data->Hours);
+	TxADCBuff[4] = (uint8_t) (_rtc_data->Days);
+	TxADCBuff[5] = (uint8_t) (CURRENT_BITS_WEEKDAY);
+	TxADCBuff[6] = (uint8_t) (_rtc_data->Month);
+	TxADCBuff[7] = (uint16_t) (_rtc_data->Year);
+
+	/* Start Transmission process */
+	HAL_GPIO_WritePin(PCA21125_SPI_CS_GPIO_Port, PCA21125_SPI_CS_Pin, GPIO_PIN_SET);
+	if (HAL_SPI_Transmit(PCA21125_SPI, TxADCBuff, 8, 100) != HAL_OK) {
+		Error_Handler();
+	}
+	HAL_GPIO_WritePin(PCA21125_SPI_CS_GPIO_Port, PCA21125_SPI_CS_Pin, GPIO_PIN_RESET);
+}
+
+void RTC_SetTime(RTC_Data* _rtc_data) {
+	RTC_Data _rtc_set_data;
+
+	/* Check the time range */	//OK
+	_rtc_data->Seconds = CHECK_SEC_MIN_RANGE(_rtc_data->Seconds);
+	_rtc_data->Minutes = CHECK_SEC_MIN_RANGE(_rtc_data->Minutes);
+	_rtc_data->Hours   = CHECK_HOURS_RANGE(_rtc_data->Hours);
+	_rtc_data->Days    = CHECK_DAYS_RANGE(_rtc_data->Days);
+	_rtc_data->Month   = CHECK_MONTHS_RANGE(_rtc_data->Month);
+	_rtc_data->Year    = CHECK_YEARS_RANGE(_rtc_data->Year);
+
+	/* Convert time to BCD data */
+	_rtc_set_data.Seconds = (((_rtc_data->Seconds/10) << 4) &0xF0) | ((_rtc_data->Seconds%10) & 0x0F);
+	_rtc_set_data.Minutes = (((_rtc_data->Minutes/10) << 4) &0xF0) | ((_rtc_data->Minutes%10) & 0x0F);
+	_rtc_set_data.Hours   = (((_rtc_data->Hours/10) << 4) &0xF0) | ((_rtc_data->Hours%10) & 0x0F);
+	_rtc_set_data.Days	  = (((_rtc_data->Days/10) << 4) &0xF0) | ((_rtc_data->Days%10) & 0x0F);
+	_rtc_set_data.Month   = (((_rtc_data->Month/10) << 4) &0xF0) | ((_rtc_data->Month%10) & 0x0F);
+	_rtc_set_data.Year    = (((_rtc_data->Year/10) << 4) &0xF0) | ((_rtc_data->Year%10) & 0x0F);
+	/* Write RTC data */
+	PCA21125RTC_WriteValue(&_rtc_set_data);
+
+}
+
+void PCA21125RTC_GetTime(RTC_Data* _rtc_data) {
+	uint8_t RxADCBuff[7]={0};
+	uint8_t TxRTCBuff[1];
+	TxRTCBuff[0]=READ_SECONDS_REGESTER_ID;
+
+	/* Read Time_Date registers */
+
+	/* Pull the CS high */
+	HAL_GPIO_WritePin(PCA21125_SPI_CS_GPIO_Port, PCA21125_SPI_CS_Pin, GPIO_PIN_SET);
+
+	if (HAL_SPI_Transmit(PCA21125_SPI, TxRTCBuff , 1, 100) != HAL_OK) {
+		Error_Handler();
+	}
+	if (HAL_SPI_Receive(PCA21125_SPI,RxADCBuff, 7, 100) != HAL_OK) {
+		Error_Handler();
+	}
+	HAL_GPIO_WritePin(PCA21125_SPI_CS_GPIO_Port, PCA21125_SPI_CS_Pin, GPIO_PIN_RESET);
+
+	_rtc_data->Seconds = (RxADCBuff[0]);
+	_rtc_data->Minutes = (RxADCBuff[1]);
+	_rtc_data->Hours = (RxADCBuff[2]);
+	_rtc_data->Days =  (RxADCBuff[3]);
+	_rtc_data->Weekday = (RxADCBuff[4]);
+	_rtc_data->Month = (RxADCBuff[5]);
+	_rtc_data->Year = (RxADCBuff[6]);
+
+	/* Convert the RTC data from RTC_BCD to Readable time */
+	RTC2ValidTime(_rtc_data);
+}
+
+uint8_t bcd2bin(uint8_t a) {
+	return (uint8_t) (((a & 0xf000) >> 12) * 1000 + ((a & 0xf00) >> 8) * 100 + ((a & 0xf0) >> 4) * 10 + (a & 0xf));
+}
+
+void RTC2ValidTime(RTC_Data* _rtc_data) {
+	_rtc_data->Seconds = (bcd2bin((_rtc_data->Seconds&0x70) >> 4) *10) + bcd2bin(_rtc_data->Seconds&0x0F);
+	_rtc_data->Minutes = (bcd2bin((_rtc_data->Minutes&0x70) >> 4) *10) + bcd2bin(_rtc_data->Minutes&0x0F);
+	_rtc_data->Hours   = (bcd2bin((_rtc_data->Hours&0x30) >> 4) *10) + bcd2bin(_rtc_data->Hours&0x0F);
+	_rtc_data->Days    = (bcd2bin((_rtc_data->Days&0x30) >> 4) *10) + bcd2bin(_rtc_data->Days&0x0F);
+	_rtc_data->Month   = (bcd2bin((_rtc_data->Month&0x10) >> 4) *10) + bcd2bin(_rtc_data->Month&0x0F);
+	_rtc_data->Year    = (bcd2bin((_rtc_data->Year&0xF0) >> 4) *10) + bcd2bin(_rtc_data->Year&0x0F);
+}
